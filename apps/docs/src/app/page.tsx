c61ed5747b10bbdb4255771625b610225a363e26
Sebastian Lugo <sebalugo@gmail.com>
2025-02-03 13:15:15 -0400

    update docs app

---
+++ apps/docs/src/app/page.tsx
@@ -0,0 +1,42 @@
+import Image, { type ImageProps } from "next/image";
+import { Button } from "@repo/ui/button";
+import styles from "./page.module.css";
+
+type Props = Omit<ImageProps, "src"> & {
+  srcLight: string;
+  srcDark: string;
+};
+
+const ThemeImage = (props: Props) => {
+  const { srcLight, srcDark, ...rest } = props;
+
+  return (
+    <>
+      <Image {...rest} src={srcLight} className="imgLight" />
+      <Image {...rest} src={srcDark} className="imgDark" />
+    </>
+  );
+};
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <ThemeImage
+          className={styles.logo}
+          srcLight="turborepo-dark.svg"
+          srcDark="turborepo-light.svg"
+          alt="Turborepo logo"
+          width={180}
+          height={38}
+          priority
+        />
+        <h1 className={styles.title}>Welcome to TurboRepo</h1>
+
+        <Button appName="docs" className={styles.secondary}>
+          Open alert
+        </Button>
+      </main>
+    </div>
+  );
+}
