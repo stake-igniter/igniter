5bde8e22bac250cb6e209ef621d0330b1da12961
Sebastian Lugo <sebalugo@gmail.com>
2025-02-24 18:31:40 -0400

    add middleman utils

---
+++ apps/middleman/src/lib/utils.ts
@@ -0,0 +1,21 @@
+export const roundAndSeparate = (
+  value: number,
+  decimalPlaces = 4,
+  defaultValue: string | number = "-"
+) => {
+  return value
+    ? parseFloat(value.toFixed(decimalPlaces)).toLocaleString(undefined, {
+        maximumFractionDigits: decimalPlaces,
+      })
+    : (defaultValue as string);
+};
+
+export const getRandomInt = (min: number, max: number) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+export function getShortAddress(address: string, length = 8) {
+  return address && address.length
+    ? address.slice(0, length) + "..." + address.slice(-length)
+    : "";
+}
