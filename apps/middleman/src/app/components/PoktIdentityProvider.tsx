b437beb02a00c88f46b10646a6e6359cf36c028a
Sebastian Lugo <sebalugo@gmail.com>
2025-02-27 18:27:46 -0400

    login remove unused code,

--- apps/middleman/src/app/components/PoktIdentityProvider.tsx
+++ apps/middleman/src/app/components/PoktIdentityProvider.tsx
@@ -4,33 +4,14 @@ import { Button } from "@igniter/ui/components/button";
 import { PocketLogo } from "@igniter/ui/assets";
 import { usePoktWalletContext } from "@/app/context/poktWallet";
 import { signIn } from "next-auth/react";
-import { createUserAndSignIn } from "../actions";
-import { useActionState, useEffect, useState } from "react";
 
 interface PoktIdentityProviderProps {
   withSignUp?: boolean;
 }
 
-interface SignUpData {
-  address: string;
-  message: string;
-  signature: string;
-  publicKey: string;
-}
-
-const PoktIdentityProvider: React.FC<PoktIdentityProviderProps> = ({
-  withSignUp = false,
-}) => {
-  const { connectWallet, poktWalletAvailable, verifyPoktIdentity, address } =
+const PoktIdentityProvider: React.FC<PoktIdentityProviderProps> = () => {
+  const { connectWallet, poktWalletAvailable, verifyPoktIdentity } =
     usePoktWalletContext();
-
-  const [state, formAction] = useActionState(createUserAndSignIn, null, "n/a");
-  const [signUpData, setSignUpData] = useState<SignUpData>({
-    address: "",
-    message: "",
-    signature: "",
-    publicKey: "",
-  });
 
   const handleLogin = async () => {
     try {
@@ -42,61 +23,22 @@ const PoktIdentityProvider: React.FC<PoktIdentityProviderProps> = ({
         message: JSON.stringify(message),
         signature,
         publicKey,
-        callbackUrl: "/",
+        redirectTo: "/app",
       });
     } catch (error) {
       console.error(error);
     }
   };
 
-  const handleSignUp = async () => {
-    try {
-      const { address } = await connectWallet();
-      const { message, signature, publicKey } =
-        await verifyPoktIdentity(address);
-
-      setSignUpData({ address, message, signature, publicKey });
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  useEffect(() => {
-    //SUBMIT FORM
-
-    const handleCreateUser = async () => {
-      try {
-        await fetch("/api/signup", {
-          method: "POST",
-          body: JSON.stringify(signUpData),
-        });
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    if (signUpData) {
-      handleCreateUser();
-    }
-  }, [signUpData]);
-
   return (
-    <div>
-      <Button
-        onClick={handleSignUp}
-        variant="secondary"
-        disabled={!poktWalletAvailable}
-      >
-        Login with POKT
-        <PocketLogo />
-      </Button>
-      <form action={formAction}>
-        <input type="hidden" name="address" value={signUpData?.address} />
-        <input type="hidden" name="message" value={signUpData?.message} />
-        <input type="hidden" name="signature" value={signUpData?.signature} />
-        <input type="hidden" name="publicKey" value={signUpData?.publicKey} />
-      </form>
-    </div>
+    <Button
+      onClick={handleLogin}
+      variant="secondary"
+      disabled={!poktWalletAvailable}
+    >
+      Login with POKT
+      <PocketLogo />
+    </Button>
   );
 };
 
