6b00f28e9ba7e0257c1e994e91170af4c3bdee28
Julio Henriquez <julio.m.henriquez@gmail.com>
2025-03-09 13:36:27 -0400

    #18 - WIP - Fixes the  eye candy gradient and text.

--- apps/middleman/src/app/(landing)/components/Hero.tsx
+++ apps/middleman/src/app/(landing)/components/Hero.tsx
@@ -12,11 +12,14 @@ export default function Hero() {
                         <span className="font-[var(--font-sans)] mt-[86px] text-[50px] text-white text-center">
                             Made Easy For
                         </span>
-                        <span className="inline-flex items-center justify-center ml-[16px] w-[177px] h-[67px] opacity-15 bg-gradient-to-b from-[#e470d9] to-[#967ff4]">
-                            <span className="font-[var(--font-mono)] text-[50px] bg-gradient-to-b from-[#e470d9] to-[#967ff4] bg-clip-text">
+                        <span className="relative inline-flex items-center justify-center ml-[16px] w-[177px] h-[67px]">
+                            <span className="font-[var(--font-mono)] text-[50px] bg-gradient-to-b from-[#e470d9] to-[#967ff4] text-transparent bg-clip-text">
                                 $POKT
                             </span>
+                            <span className="absolute w-[177px] h-[67px] opacity-15 bg-gradient-to-b from-[#e470d9] to-[#967ff4]">
+                            </span>
                         </span>
+
                     </div>
                     <span className="font-[var(--font-sans)] text-[18px] mt-[35px] leading-[1.83] text-[var(--color-white-3)] text-center">
                       Effortlessly stake your tokens across multiple providers using Igniter. Ignite your potential and make your tokens work for you.
