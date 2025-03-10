8fc14b4fa93ff2edad49c5ef9c27c091f77bc719
Sebastian Lugo <sebalugo@gmail.com>
2025-03-09 22:21:43 -0500

    expand default session User type

--- apps/middleman/src/types.ts
+++ apps/middleman/src/types.ts
@@ -1,5 +1,33 @@
+import NextAuth, { DefaultSession } from "next-auth";
+import { UserRole } from "./db/schema";
+
 declare global {
   interface Window {
     pocketNetwork: any;
+  }
+}
+
+declare module "next-auth" {
+  /**
+   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
+  interface Session {
+    user: {
+      /** The user's role. */
+      role: UserRole;
+      identity: string;
+    } & DefaultSession["user"];
+  }
+  interface User {
+    role: string;
+  }
+}
+
+declare module "next-auth/jwt" {
+  /** Returned by the `jwt` callback and `getToken`, when using JWT sessions */
+  interface JWT {
+    /** The user's role */
+    role: UserRole;
+    identity: string;
   }
 }
