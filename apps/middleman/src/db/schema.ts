9a3f4cd641663fd0bdfac0ae312c09a6e46e4560
Sebastian Lugo <sebalugo@gmail.com>
2025-03-13 21:35:12 -0500

    add activity and transaction to schema

--- apps/middleman/src/db/schema.ts
+++ apps/middleman/src/db/schema.ts
@@ -1,6 +1,8 @@
+import { relations } from "drizzle-orm";
 import {
   boolean,
   decimal,
+  AnyPgColumn,
   integer,
   pgEnum,
   pgTable,
@@ -31,6 +33,49 @@ export enum BlockchainProtocol {
   Shannon = "shannon",
 }
 
+export enum ActivityType {
+  Stake = "stake",
+  Unstake = "unstake",
+  Upstake = "upstake",
+  OperationalFunds = "operational_funds",
+}
+
+export enum TransactionType {
+  Stake = "stake",
+  Unstake = "unstake",
+  Send = "send",
+}
+
+export enum ActivityStatus {
+  Pending = "pending",
+  Success = "success",
+  Failure = "failure",
+}
+
+export enum TransactionStatus {
+  Pending = "pending",
+  Success = "success",
+  Failure = "failure",
+  NotExecuted = "not_executed",
+}
+
+export const activityTypeEnum = pgEnum("type", enumToPgEnum(ActivityType));
+
+export const activityStatusEnum = pgEnum(
+  "status",
+  enumToPgEnum(ActivityStatus)
+);
+
+export const transactionTypeEnum = pgEnum(
+  "type",
+  enumToPgEnum(TransactionType)
+);
+
+export const transactionStatusEnum = pgEnum(
+  "status",
+  enumToPgEnum(TransactionStatus)
+);
+
 export const roleEnum = pgEnum("role", enumToPgEnum(UserRole));
 
 export const chainIdEnum = pgEnum("chain_ids", enumToPgEnum(ChainId));
@@ -49,7 +94,7 @@ export const usersTable = pgTable("users", {
   updatedAt: timestamp().defaultNow(),
 });
 
-export type User = typeof usersTable.$inferSelect
+export type User = typeof usersTable.$inferSelect;
 
 export const providersTable = pgTable("providers", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
@@ -58,7 +103,7 @@ export const providersTable = pgTable("providers", {
   url: varchar({ length: 255 }).notNull(),
   enabled: boolean().notNull(),
   visible: boolean().notNull().default(true),
-  fee: decimal().notNull().default('1.00'),
+  fee: decimal().notNull().default("1.00"),
   domains: text().array().default([]),
   minimumStake: integer().notNull().default(0),
   operationalFunds: integer().notNull().default(5),
@@ -85,3 +130,65 @@ export const applicationSettingsTable = pgTable("application_settings", {
 });
 
 export type ApplicationSettings = typeof applicationSettingsTable.$inferSelect;
+
+export const activityTable = pgTable("activity", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  type: activityTypeEnum().notNull(),
+  status: activityStatusEnum().notNull(),
+  seenOn: timestamp(),
+  createdAt: timestamp().defaultNow(),
+  updatedAt: timestamp().defaultNow(),
+});
+
+export const activityTransactionRelation = relations(
+  activityTable,
+  ({ many }) => ({
+    transactions: many(transactionsTable),
+  })
+);
+
+export type Activity = typeof activityTable.$inferSelect;
+
+export const transactionsTable = pgTable("transactions", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  hash: varchar({ length: 255 }),
+  type: transactionTypeEnum().notNull(),
+  status: transactionStatusEnum().notNull(),
+  executionHeight: integer(),
+  executionTimestamp: timestamp(),
+  verificationHeight: integer(),
+  verificationTimestamp: timestamp(),
+
+  //Self-referencing foreign key workaround: https://orm.drizzle.team/docs/indexes-constraints#foreign-key
+  dependsOn: integer().references((): AnyPgColumn => transactionsTable.id),
+
+  signedPayload: varchar(),
+  signatureTimestamp: timestamp().notNull(),
+  activityId: integer()
+    .notNull()
+    .references(() => activityTable.id),
+  createdAt: timestamp().defaultNow(),
+  updatedAt: timestamp().defaultNow(),
+});
+
+export const transactionsActivityRelation = relations(
+  transactionsTable,
+  ({ one }) => ({
+    activity: one(activityTable, {
+      fields: [transactionsTable.activityId],
+      references: [activityTable.id],
+    }),
+  })
+);
+
+export const transactionsDependsOnRelation = relations(
+  transactionsTable,
+  ({ one }) => ({
+    dependsOn: one(transactionsTable, {
+      fields: [transactionsTable.dependsOn],
+      references: [transactionsTable.id],
+    }),
+  })
+);
+
+export type Transaction = typeof transactionsTable.$inferSelect;
