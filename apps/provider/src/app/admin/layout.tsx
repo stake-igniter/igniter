2beb7bbdac708dd5551896910ad0853d8d709298
Sebastian Lugo <sebalugo@gmail.com>
2025-04-02 20:08:08 -0500

    provider layout refactor for redirects

---
+++ apps/provider/src/app/admin/layout.tsx
@@ -0,0 +1,33 @@
+import { Jost, Overpass_Mono } from "next/font/google";
+
+const jost = Jost({
+  variable: "--font-jost",
+  weight: ["400", "600", "500", "700"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
+const overpass_mono = Overpass_Mono({
+  variable: "--font-overpass-mono",
+  weight: ["400", "600", "500", "700"],
+  style: ["normal"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${overpass_mono.variable} ${jost.variable} overflow-hidden`}
+      suppressHydrationWarning
+    >
+      <body>{children}</body>
+    </html>
+  );
+}
