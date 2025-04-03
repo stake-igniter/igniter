9ac5ab4c85b0cb91c7d2bd7e39c886b885357dda
Sebastian Lugo <sebalugo@gmail.com>
2025-04-02 20:07:48 -0500

    remove redirect in signIn callback for now

--- apps/middleman/src/auth.config.ts
+++ apps/middleman/src/auth.config.ts
@@ -1,27 +1,23 @@
 import { NextAuthConfig } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import { bootstrapStatus } from "@/lib/services/bootstrap";
-import {SiwpMessage} from "@poktscan/vault-siwp";
+import { SiwpMessage } from "@poktscan/vault-siwp";
 
 const authConfig: NextAuthConfig = {
   providers: [Credentials],
   callbacks: {
-    async signIn({ user, credentials}) {
+    async signIn({ user, credentials }) {
       const isBootstrapped = await bootstrapStatus();
 
       const { address } = new SiwpMessage(
         JSON.parse((credentials?.message || "{}") as string)
       );
 
-       if (!isBootstrapped && address !== process.env.OWNER_IDENTITY) {
-         return '/auth/error?error=OwnerOnly';
-       }
-
-      if (!isBootstrapped && address === process.env.OWNER_IDENTITY) {
-        return '/admin/setup';
+      if (!isBootstrapped && address !== process.env.OWNER_IDENTITY) {
+        return "/auth/error?error=OwnerOnly";
       }
 
-       return true;
+      return true;
     },
     async session({ session, token }) {
       // TODO: Remove ts-ignore when we figure out how to set the expected user type across next-auth
