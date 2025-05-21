c31cdb772279c5fd61a6b38919ea61c507b6a04f
Julio Henriquez <julio.m.henriquez@gmail.com>
2025-05-21 16:57:34 -0400

    Add detailed logging to provider and supplier APIs
    Added console log statements to enhance traceability and debugging for provider and supplier API flows. Logs include request handling, payload validation, provider loading, signature validation, and response generation processes.

--- apps/provider/src/app/api/suppliers/route.ts
+++ apps/provider/src/app/api/suppliers/route.ts
@@ -19,34 +19,44 @@ export async function OPTIONS() {
 
 export async function POST(request: Request): Promise<NextResponse<APIResponse<Supplier[] | null>>> {
   try {
+    console.log('Received a request for suppliers');
     const isBootstrappedResponse = await ensureApplicationIsBootstrapped();
     const settings = await getApplicationSettings();
 
     if (isBootstrappedResponse instanceof NextResponse) {
+      console.log('Application is not bootstrapped. Exiting.');
       return isBootstrappedResponse;
     }
 
     const delegatorIdentity = request.headers.get(REQUEST_IDENTITY_HEADER);
 
     if (!delegatorIdentity) {
+      console.log(`Invalid request. Delegator identity was not provided. REQUEST_IDENTITY_HEADER: ${REQUEST_IDENTITY_HEADER} is required.`);
       return NextResponse.json({error: `Invalid request. Delegator identity was not provided. REQUEST_IDENTITY_HEADER: ${REQUEST_IDENTITY_HEADER} is required.`}, {status: 400});
     }
 
+    console.log('Validating signature...');
     const signatureValidationResponse = await validateRequestSignature<SupplierStakeRequest>(request);
 
     if (signatureValidationResponse instanceof NextResponse) {
+      console.log('Signature validation failed. Exiting.');
       return signatureValidationResponse;
     }
 
+    console.log('Signature validation successful.');
     const {data} = signatureValidationResponse;
 
+
     if (!data || !data.items.length) {
+      console.log('Invalid request. Empty stake distribution.');
       return NextResponse.json({error: "Invalid request. Empty stake distribution."}, {status: 400});
     }
 
+    console.log('Generating addresses...');
     const response = await getSupplierStakeConfigurations(data, delegatorIdentity, settings);
 
     if (!response || response.length === 0) {
+      console.log('No addresses available');
       return NextResponse.json(
         {error: "No addresses available"},
         {
@@ -58,6 +68,7 @@ export async function POST(request: Request): Promise<NextResponse<APIResponse<S
       );
     }
 
+    console.log('Returning addresses');
     return NextResponse.json({
       data: response,
     }, {
