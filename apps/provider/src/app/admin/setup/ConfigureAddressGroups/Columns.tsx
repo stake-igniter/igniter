29d7ee1ba71ba8f734e408a2702f2c921fb951d5
Julio Henriquez <julio.m.henriquez@gmail.com>
2025-07-02 19:50:35 -0400

    Update apps/provider/src/app/admin/setup/ConfigureAddressGroups/Columns.tsx
    Co-authored-by: Jorge S. Cuesta <jckpo2@gmail.com>

--- apps/provider/src/app/admin/setup/ConfigureAddressGroups/Columns.tsx
+++ apps/provider/src/app/admin/setup/ConfigureAddressGroups/Columns.tsx
@@ -43,7 +43,6 @@ export const columns: ColumnDef<AddressGroupWithDetails>[] = [
     accessorKey: "linkedAddresses",
     header: "Linked Addresses",
     cell: ({ row }) => {
-
       const linkedAddresses = row.getValue("linkedAddresses") as string[]
       return linkedAddresses && linkedAddresses.length > 0 ? `${linkedAddresses.length} Linked Addresses` : "No Linked Addresses";
     }
