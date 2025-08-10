78021da68466c945017561e270faed48b238336f
Jorge S. Cuesta <jckpo2@gmail.com>
2025-08-09 22:42:22 -0400

    Add Docker Compose stacks for dependencies, provider, and middleman
    Introduce Docker Compose configurations to run PostgreSQL, Temporal, provider, and middleman services locally. Includes environment variable samples, initialization scripts, and detailed README documentation for setup and usage.

---
+++ docker-compose/dependencies/postgresql/init.sql
@@ -0,0 +1,8 @@
+-- Create the middleman database
+CREATE DATABASE "middleman";
+
+-- Create the provider database
+CREATE DATABASE "provider";
+
+-- Create the temporal database
+CREATE DATABASE "temporal";
---
+++ docker-compose/dependencies/temporal/initialize.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+temporal operator namespace create provider
+
+temporal operator namespace create middleman
